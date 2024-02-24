388dcfaeacfb98e525210a695860ccc1dab04049
Paul Breslin <paul.breslin@outlook.com>
2024-02-24 18:06:57 +0000

    feat: adds initial files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,214 @@
+import { useState, useRef } from "react";
 import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
+import { useHotkeys } from "react-hotkeys-hook";
+import Fuse from "fuse.js";
 
-const inter = Inter({ subsets: ["latin"] });
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+
+const poppinsLight = Poppins({ subsets: ["latin"], weight: ["300"] });
+const poppinsMedium = Poppins({ subsets: ["latin"], weight: ["500"] });
+
+const assets = [
+  {
+    name: "Bitcoin",
+    price: "£41,134",
+    change: "121%",
+    marketCap: "£790B",
+    volume: "£19B",
+    data: [
+      { date: "2023-02-01", price: 30000 },
+      { date: "2023-03-01", price: 28000 },
+      { date: "2023-04-01", price: 32000 },
+      { date: "2023-05-01", price: 34000 },
+      { date: "2023-06-01", price: 33000 },
+      { date: "2023-07-01", price: 35000 },
+      { date: "2023-08-01", price: 36000 },
+      { date: "2023-09-01", price: 37000 },
+      { date: "2023-10-01", price: 38000 },
+      { date: "2023-11-01", price: 40000 },
+      { date: "2023-12-01", price: 39000 },
+      { date: "2024-01-01", price: 41000 },
+    ],
+  },
+  {
+    name: "Ethereum",
+    price: "£2,050",
+    change: "84%",
+    marketCap: "£278B",
+    volume: "£11B",
+    data: [
+      { date: "2023-02-01", price: 1500 },
+      { date: "2023-03-01", price: 1450 },
+      { date: "2023-04-01", price: 1550 },
+      { date: "2023-05-01", price: 1600 },
+      { date: "2023-06-01", price: 1700 },
+      { date: "2023-07-01", price: 1650 },
+      { date: "2023-08-01", price: 1750 },
+      { date: "2023-09-01", price: 1800 },
+      { date: "2023-10-01", price: 1900 },
+      { date: "2023-11-01", price: 2000 },
+      { date: "2023-12-01", price: 1950 },
+      { date: "2024-01-01", price: 2050 },
+    ],
+  },
+  {
+    name: "Tether",
+    price: "£1.0",
+    change: "-0.07",
+    marketCap: "£77B",
+    volume: "£40B",
+    data: [
+      { date: "2023-02-01", price: 1.0 },
+      { date: "2023-03-01", price: 1.01 },
+      { date: "2023-04-01", price: 1.0 },
+      { date: "2023-05-01", price: 0.99 },
+      { date: "2023-06-01", price: 1.0 },
+      { date: "2023-07-01", price: 1.0 },
+      { date: "2023-08-01", price: 1.01 },
+      { date: "2023-09-01", price: 1.0 },
+      { date: "2023-10-01", price: 1.0 },
+      { date: "2023-11-01", price: 0.99 },
+      { date: "2023-12-01", price: 1.0 },
+      { date: "2024-01-01", price: 1.0 },
+    ],
+  },
+  {
+    name: "BNP",
+    price: "£410.57",
+    change: "-0.07",
+    marketCap: "£77B",
+    volume: "£40B",
+    data: [
+      { date: "2023-02-01", price: 300 },
+      { date: "2023-03-01", price: 320 },
+      { date: "2023-04-01", price: 310 },
+      { date: "2023-05-01", price: 330 },
+      { date: "2023-06-01", price: 340 },
+      { date: "2023-07-01", price: 350 },
+      { date: "2023-08-01", price: 370 },
+      { date: "2023-09-01", price: 360 },
+      { date: "2023-10-01", price: 380 },
+      { date: "2023-11-01", price: 400 },
+      { date: "2023-12-01", price: 390 },
+      { date: "2024-01-01", price: 410 },
+    ],
+  },
+  {
+    name: "Solana",
+    price: "£140.28",
+    change: "-0.07",
+    marketCap: "£77B",
+    volume: "£40B",
+    data: [
+      { date: "2023-02-01", price: 90 },
+      { date: "2023-03-01", price: 85 },
+      { date: "2023-04-01", price: 100 },
+      { date: "2023-05-01", price: 95 },
+      { date: "2023-06-01", price: 110 },
+      { date: "2023-07-01", price: 105 },
+      { date: "2023-08-01", price: 120 },
+      { date: "2023-09-01", price: 115 },
+      { date: "2023-10-01", price: 130 },
+      { date: "2023-11-01", price: 125 },
+      { date: "2023-12-01", price: 135 },
+      { date: "2024-01-01", price: 140 },
+    ],
+  },
+];
 
 export default function Home() {
+  const inputRef = useRef(null);
+
+  const [selectedAsset, setSelectedAsset] = useState("Bitcoin");
+  const [searchText, setSearchText] = useState("");
+
+  useHotkeys("/", (event) => {
+    event.preventDefault();
+    inputRef.current.focus();
+  });
+
+  const fuse = new Fuse(assets, {
+    keys: ["name"],
+    threshold: 0.2,
+  });
+
+  const results = fuse.search(searchText);
+
+  const assetList = searchText.length
+    ? results.map((result) => result.item)
+    : assets;
+
+  const currentAsset =
+    searchText.length && assetList.length === 1
+      ? assetList[0]
+      : assets.find((asset) => asset.name == selectedAsset);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div className="flex flex-nowrap h-screen">
+      <aside
+        className={`${poppinsMedium.className} w-[320px] min-w-[320px] bg-[#232323] p-4`}
+      >
+        <input
+          ref={inputRef}
+          className="border p-2 rounded bg-[#181A1B] w-full"
+          placeholder="Search for assets"
+          value={searchText}
+          onChange={(value) => setSearchText(event.target.value)}
         />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <h2 className="pt-8 pb-4">Assets</h2>
+        {assetList.map((asset, index) => {
+          return (
+            <div
+              key={asset.name}
+              className={`flex place-content-between cursor-pointer hover:text-[#ff6f1a] py-2`}
+              onClick={() => setSelectedAsset(asset.name)}
+            >
+              <div>{asset.name}</div>
+              <div>{asset.price}</div>
+            </div>
+          );
+        })}
+        {Boolean(searchText.length) && !assetList.length && (
+          <div>No assets found</div>
+        )}
+      </aside>
+      <main
+        className={`${poppinsLight.className} flex-grow flex justify-center p-4`}
+      >
+        <div className="max-w-[800px] w-full p-4">
+          <h1 className={`${poppinsMedium.className} text-xl pb-8`}>
+            {currentAsset.name}
+          </h1>
+          <LineChart
+            width={750}
+            height={400}
+            data={currentAsset?.data}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="date" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line
+              type="monotone"
+              dataKey="price"
+              stroke="#8884d8"
+              activeDot={{ r: 8 }}
+            />
+          </LineChart>
+        </div>
+      </main>
+    </div>
   );
 }
